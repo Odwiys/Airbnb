99f19c9161362decdbc73e9feb237c3d353466a4
Sabrina lee <sabrinaleess6@gmail.com>
2023-06-15 13:24:48 +0800

    deleted unnecessary migration file for devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+<<<<<<< Updated upstream
+=======
+  # before_action :authenticate_user!
+>>>>>>> Stashed changes
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
