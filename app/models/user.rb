27e1f226e8278c900d7a9904b458cce2067d33c9
mathias loh <matlohck@gmail.com>
2023-06-15 12:28:42 +0800

    updated user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :wishlists
+  has_many :listings
+  has_many :bookmarks, through: :listings
+  has_many :reservations
+
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true
+  validates :phone_number, presence: true, uniqueness: true
 end
