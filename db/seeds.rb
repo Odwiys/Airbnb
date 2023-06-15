a18f66196d4b5d93c4379c3c7229cd13c85638c7
Sabrina lee <sabrinaleess6@gmail.com>
2023-06-15 13:07:56 +0800

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,15 +6,15 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-puts 'Creating Users...'
-user_host = User.new(
-	name: "Host Name"
+# puts 'Creating Users...'
+# user_host = User.new(
+# 	name: "Host Name"
 
-)
-user_host.save!
+# )
+# user_host.save!
 
-user_guest = User.new(
-	name: "Guest Name"
-)
-user_guest.save!
-puts 'Users Created!'
+# user_guest = User.new(
+# 	name: "Guest Name"
+# )
+# user_guest.save!
+# puts 'Users Created!'
