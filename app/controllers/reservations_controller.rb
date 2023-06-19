732dd704e917f6fa3898d43ab7e45e5dfe6312f9
Anabelle Ho <anabelleho@hotmail.com>
2023-06-19 15:07:47 +0800

    added reservations

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,2 +1,11 @@
 class ReservationsController < ApplicationController
+  def new
+    raise
+    @listing = Listing.find(params[:listing_id])
+    @reservation = Reservation.new
+  end
+
+  def create
+
+  end
 end
