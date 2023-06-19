f0e8e297b3c920bceb926c7074e7f8f587f955c3
mathias loh <matlohck@gmail.com>
2023-06-19 15:07:53 +0800

    set reservations

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,2 +1,14 @@
 class ReservationsController < ApplicationController
+  # before_action :set_reservation, only: %i[show]
+
+  def show
+    @listing = Listing.find(params[listing_id])
+    @reservations = listing.reservations
+  end
+
+  # private
+
+  # def set_reservation
+  #   @reservation = Reservation.find(params[:id])
+  # end
 end
