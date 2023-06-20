c461a90064d0082baefe9db3e5a0b5d6b7c6ae69
Sabrina lee <sabrinaleess6@gmail.com>
2023-06-20 15:58:36 +0800

    edit map button

--- db/schema.rb
+++ db/schema.rb
@@ -59,8 +59,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_20_054314) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.float "latitude"
-    t.float "longitude"
     t.index ["user_id"], name: "index_listings_on_user_id"
   end
 
@@ -71,7 +69,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_20_054314) do
     t.bigint "listing_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.boolean "status", default: nil
+    t.boolean "status"
     t.index ["listing_id"], name: "index_reservations_on_listing_id"
     t.index ["user_id"], name: "index_reservations_on_user_id"
   end
